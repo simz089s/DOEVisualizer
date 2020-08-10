3c5e3af57caf3b92756ddddc21e341a8b411eec0
Simon Zheng <simz97@outlook.com>
2020-08-10 04:00:06 -0400

    Implement plotting and graphing variable pairs

--- src/process_data.py
+++ src/process_data.py
@@ -2,7 +2,8 @@ import sqlite3
 import numpy as np
 import scipy as sp
 import pandas as pd
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+# import pylab
 # import sklearn
 # import torch
 
@@ -18,6 +19,21 @@ def fishbone(data):
     return
 
 ishikawa = fishbone
+
+
+def plot_pair(df, x, y, title):
+    graph = df.plot(
+        x=x,
+        y=y,
+        kind='scatter',
+        title=title
+        # label=[df.columns.values[i] for i in (x, y)]
+        )
+    graph.plot()
+    # ax = plt.gca()
+    # x_df.plot(x=None, y=None, kind='scatter', label=x_df.name, ax=ax, color='lime')
+    # y_df.plot(x=None, y=None, kind='scatter', label=y_df.name, ax=ax, color='red')
+    # plt.show()
 
 
 def main():
