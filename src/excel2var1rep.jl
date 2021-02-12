f5b3ac7fdc6e1668b14627d722e21dfba73cba67
Simon Zheng <simz97@outlook.com>
2021-02-12 18:29:46 -0500

    Prototype 1

--- src/excel2var1rep.jl
+++ src/excel2var1rep.jl
@@ -6,7 +6,7 @@ using CSV, DataFrames
 
 # using ForwardDiff
 # using AutoGrad
-using GLM, StatsModels
+# using GLM, StatsModels
 
 using Plots, StatsPlots
 # using Gadfly
@@ -22,26 +22,33 @@ rename!(df, titles)
 delete!(df, 1)
 df[!, :] = parse.(Float64, df[!, :])
 
-xs = select(df, titles[1:3])
-ys = select(df, titles[4:6])
+NUM_VARS = 3
+NUM_RESPS = 3
+NUM_LVLS = 3
+low = 1:3
+mid = 4:6
+high = 7:9
 
-f = @formula(y_yield ~ 1 + x_stime + x_t + x_atime)
-model = glm(f, select(df, 1:4), Normal(), IdentityLink())
+# f = @formula(y_yield ~ 1 + x_stime + x_t + x_atime)
+# model = glm(f, select(df, 1:4), Normal(), IdentityLink())
 
-# @df df plot(xs[1], xs[2], ys[1], line=:scatter, zcolor=ys[1], xaxis="S time", yaxis="Temp.", lab="Yield")
-# @df df plot!(xs[1], xs[2], predict(model), line=:surface, label="y_yield ~ 1 + x_stime + x_t + x_atime")
-# @df df plot!(xs[1], xs[2], predict(model), line=:line, label="y_yield ~ 1 + x_stime + x_t + x_atime")
-
-# @df df plot(xs[1], xs[3], ys[1], line=:scatter, zcolor=ys[1], xaxis="S time", yaxis="Temp.", lab="Yield")
-# @df df plot!(xs[1], xs[3], predict(model), line=:surface, label="y_yield ~ 1 + x_stime + x_t + x_atime")
-# @df df plot!(xs[1], xs[3], predict(model), line=:line, label="y_yield ~ 1 + x_stime + x_t + x_atime")
-
-# @df df plot(xs[2], xs[3], ys[1], line=:scatter, zcolor=ys[1], xaxis="S time", yaxis="Temp.", lab="Yield")
-# @df df plot!(xs[2], xs[3], predict(model), line=:surface, label="y_yield ~ 1 + x_stime + x_t + x_atime")
-# @df df plot!(xs[2], xs[3], predict(model), line=:line, label="y_yield ~ 1 + x_stime + x_t + x_atime")
-
-for (idx, title) ∈ enumerate(names(xs))
-    display(@df df plot(xs[1], xs[2], xs[3], line=:scatter, zcolor=ys[idx], markersize=10, c=ColorGradient([:red,:yellow,:green]), xaxis="S time", yaxis="Temp.", zaxis="A time", lab=title))
+for (idx, title) ∈ enumerate(names(df[4:4]))
+    df_sorted_by_y = select(df, titles[1:3], title)
+    df_sorted_by_y = sort!(df_sorted_by_y, title)
+    graph = @df df plot(
+        df_sorted_by_y[low, 1], df_sorted_by_y[low, 2], df_sorted_by_y[low, 3],
+        line=:scatter, zcolor=df_sorted_by_y[low, title], markersize=10, markershape=:circle, c=cgrad([:red3, :red3, :red3]),# marker=([:hex :d], 12, 0.8, Plots.stroke(3, :gray)),
+        xaxis=titles[1], yaxis=titles[2], zaxis=titles[3], lab="Low", colorbar=false, arrow=true,
+        reuse=false)
+    graph = @df df plot!(
+        df_sorted_by_y[mid, 1], df_sorted_by_y[mid, 2], df_sorted_by_y[mid, 3],
+        line=:scatter, zcolor=df_sorted_by_y[mid, title], markersize=10, markershape=:utriangle, c=cgrad([:yellow, :yellow, :yellow]),
+        lab="Medium")
+    graph = @df df plot!(
+        df_sorted_by_y[high, 1], df_sorted_by_y[high, 2], df_sorted_by_y[high, 3],
+        line=:scatter, zcolor=df_sorted_by_y[high, title], markersize=10, markershape=:square, c=cgrad([:green, :green, :green]),
+        lab="High")
+    display(graph)
 end
 
 # end
