2a944648d6cf7f5ecdaf3c4bdc7df74a246e4b91
Simon Zheng <simz97@outlook.com>
2021-02-15 21:04:36 -0500

    Checkpoint

--- src/excel2var1rep.jl
+++ src/excel2var1rep.jl
@@ -3,15 +3,10 @@
 # using PackageCompiler
 
 using CSV, DataFrames
-
-# using ForwardDiff
-# using AutoGrad
 # using GLM, StatsModels
-
-using Plots, StatsPlots
-# using Gadfly
-# using Makie
+using Plots#, StatsPlots
 pyplot()
+# using Makie
 
 df = CSV.File("res/heat_treatement_data_2.csv") |> DataFrame
 
@@ -34,20 +29,34 @@ df[!, :] = parse.(Float64, df[!, :])
 # f = @formula(y_yield ~ 1 + x_stime + x_t + x_atime)
 # model = glm(f, select(df, 1:4), Normal(), IdentityLink())
 
+# mutable struct NoDepthShade end
+# @recipe function no_depthshade(::NoDepthShade)
+#     depthshade --> false
+# end
+
 graphs = Array{Plots.Plot{Plots.PyPlotBackend}, 1}(undef, NUM_RESPS)
 for (idx, title) ∈ enumerate(names(select(df, 4:6, copycols=false)))
-    df_sorted_by_y = select(df, titles[1:3], title)
-    df_sorted_by_y = sort!(df_sorted_by_y, title)
-    graphs[idx] = @df df plot(
-        df_sorted_by_y[1], df_sorted_by_y[2], df_sorted_by_y[3],
-        line=:scatter, zcolor=df_sorted_by_y[title], markersize=10, markershape=:circle, c=cgrad([:red3, :yellow, :green]),
-        lab="Low", xaxis=titles[1], yaxis=titles[2], zaxis=titles[3],
-        extra_kwargs=Dict(:series => Dict("depthshade" => false)),
-        reuse=false
+    graphs[idx] = scatter3d(
+        df[1], df[2], df[3],
+        zcolor=df[title], markersize=10, markershape=:circle, c=cgrad([:red3, :yellow, :green]),
+        title="$(title) ~ $(titles[1]) + $(titles[2]) + $(titles[3])", lab=title, xaxis=titles[1], yaxis=titles[2], zaxis=titles[3],
+        seriestype=:scatter3d, line=:scatter3d,
+        # extra_kwargs=Dict(:series => Dict(:depthshade => false), :plot => Dict(:depthshade => false), :subplot => Dict(:depthshade => false)),
+        # depthshade=false,
+        # arrow=arrow(:closed, :both),
+        reuse=false,
     )
+    graphs[idx] = scatter3d!(graphs[idx], depthshade=false)
+    # get!(graphs[idx][:extra_kwargs], :depthshade, false)
+    # get(graphs[idx][:extra_kwargs][:plot], :depthshade, false)
+    # get(graphs[idx][:extra_kwargs][:subplot], :depthshade, false)
+    # get(graphs[idx][:extra_kwargs][:series], :depthshade, false)
 end
-display(plot(graphs...))
+# display(scatter3d(graphs..., extra_kwargs=Dict( :plot => Dict(:depthshade=>false), :subplot => Dict(:depthshade=>false), :series => Dict(:depthshade=>false) )))
+display(scatter3d(graphs...))
 
 # graph = @df df plot(df_sorted_by_y[1], df_sorted_by_y[2], predict(model), line=:surface)
+
+# close("all")
 
 # end
