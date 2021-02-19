0d13edff2273cf965370e546b9eec2aff8bebf09
Simon Zheng <simz97@outlook.com>
2021-02-19 03:01:13 -0500

    Hacky way of scaling axes

--- src/excel2var1rep.jl
+++ src/excel2var1rep.jl
@@ -24,26 +24,41 @@ df[!, :] = parse.(Float64, df[!, :])
 # f = @formula(y_yield ~ 1 + x_stime + x_t + x_atime)
 # model = glm(f, select(df, 1:4), Normal(), IdentityLink())
 
-s = fig, axes, obj = scatter(
-    df[1], df[2], df[3],
-    # colors = (colors, colors, colors),
-    markersize = 500,
+x = df[1]
+y = df[2]
+z = df[3]
+
+scene, layout = layoutscene()
+
+s = layout[1, 1] = LScene(scene)
+scatter!(
+    s,
+    x, .1 * y, z,
+    markersize = 300, marker = :circle,
+    colormap = :RdYlGn_3,# colorrange = 1:80,
+    # scale_plot = true,
+    show_axis = true,
     # transparency = true, alpha = 0.1,
     # limits = FRect3D( (3, 140, 2), (4, 40, 3) ),
-    # scale = (1, 2, 1),
-    # axis3d = (
-    #     # names = (
-    #     #     axisnames = ("x", "y", "z"),
-    #     # ),
-    #     # scale = (1, .05, 1),
-    #     # scale_plot = true,
-    #     ticks = (
-    #         ranges = Node((3:7, 140:180, 2:5)),
-    #     ),
+    # scale = (1, 1, 1),
+    # axis = (
+        # names = (
+        #     axisnames = ("x", "y", "z"),
+        # ),
+        # scale = (1, .05, 1),
+        # scale_plot = true,
+        # ticks = (
+        #     ranges = Node((3:7, 140:180, 2:5)),
+        # ),
+        # grid = (
+        #     linewidth = (0, .5, 0),
+        # ),
     # ),
 )
-# yticks!(s, ytickrange=80)
+# scale!(scene, 1, 1/100, 1)
+yticks!(s.scene, yticklabels=string.(y))
 
-fig
+# fig
+display(scene)
 
 # end
