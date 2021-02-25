cea6c3f07ab4d04a9e45c7a914032a989538b71b
Simon Zheng <simz97@outlook.com>
2021-02-24 20:00:04 -0500

    Some refactoring

--- src/excel2var1rep.jl
+++ src/excel2var1rep.jl
@@ -4,19 +4,19 @@
 
 using CSV, DataFrames
 using Colors, Statistics
-using GLMakie
+using GLMakie, AbstractPlotting
 # using GLM, StatsModels
 
 function read_data(filename)
     df = CSV.File(filename) |> DataFrame
-    
+
     # First row after title should be indicating if the column is a variable or response
     types = df[1, :]
     num_vars = count(t -> !ismissing(t) && t == "variable", types)
     num_resps = count(t -> !ismissing(t) && t == "response", types)
     # NUM_LVLS = 
     num_rows = nrow(df) - 1 # Exclude row indicating if it is a variable or response column
-    
+
     select!(df, Not(1)) # TODO: clarify if (redundant) index row will always be there
     # titles = replace.(["$(x) ($(y))" for (x, y) in zip(names(df), df[1, :])], " "=>"_")
     titles = [:x_stime, :x_t, :x_atime, :y_yield, :y_str, :y_elong] # TODO: find better way to title
@@ -47,7 +47,7 @@ function get_ranges(x, y, z)
     ext_x = extrema(x)
     ext_y = extrema(y)
     ext_z = extrema(z)
-    
+
     # Scale data to data/interval so that the plot is unit/equal sized
     scal_ext_x = ext_x ./ range_x
     scal_ext_y = ext_y ./ range_y
@@ -58,7 +58,61 @@ function get_ranges(x, y, z)
         scal_ext_x, scal_ext_y, scal_ext_z
 end
 
-function graph()
+# Draw points and coordinates
+function create_points_coords(s, x, y, z, range_x, range_y, range_z, scal_x, scal_y, scal_z, scal_plot_unit, colors)
+    for (i, col) in enumerate(colors)
+        scatter!(
+            s,
+            scal_x[i:i], scal_y[i:i], scal_z[i:i],
+            markersize = scal_plot_unit * 5, marker = :circle,
+            color = col,
+            show_axis = true,
+            camera = cam3d!,
+        )
+        text!(
+            s,
+            "$((x[i], y[i], z[i]))",
+            position = Point3f0(scal_x[i]+.5/range_x, scal_y[i]+.1/range_y, scal_z[i]+.2/range_z),
+            textsize = scal_plot_unit / 500,
+            color = :black,
+            rotation = 3.15,
+        )
+    end
+end
+
+# Draw grid
+# TODO: probably use some permutation function to make it more elegant
+function create_grid(s, scal_uniq_var_vals, num_vars, n_uniq_var_vals)
+    for var_dim_idx in 1:num_vars # scal_uniq_var_vals index of the dimension that will draw the line
+        # scal_uniq_var_vals index of the other invariant dimensions
+        invar_data_dim_idx1 = mod1(var_dim_idx + 1, 3)
+        invar_data_dim_idx2 = mod1(var_dim_idx + 2, 3)
+        for line_idx in 1:n_uniq_var_vals
+            for line_idx2 in 1:n_uniq_var_vals
+                invar_data_dim1 = fill(scal_uniq_var_vals[invar_data_dim_idx1][line_idx], n_uniq_var_vals)
+                invar_data_dim2 = fill(scal_uniq_var_vals[invar_data_dim_idx2][line_idx2], n_uniq_var_vals)
+
+                # Plot function takes in order x,y,z
+                data = Array{Array{Float64, 1}, 1}(undef, 3)
+                data[var_dim_idx] = scal_uniq_var_vals[var_dim_idx]
+                data[invar_data_dim_idx1] = invar_data_dim1
+                data[invar_data_dim_idx2] = invar_data_dim2
+
+                lines!(
+                    s,
+                    data[1], data[2], data[3],
+                    linestyle = :dash,
+                    transparency = true,
+                    color = RGBA(0., 0., 0., .4),
+                    # color = colors,
+                    show_axis = true,
+                )
+            end
+        end
+    end
+end
+
+function create_plots()
     df, titles, types, num_vars, num_resps, num_rows = read_data("res/heat_treatement_data_2.csv") # TODO: better way to get filename/path
 
     x, y, z, n = get_xyzn(df)
@@ -68,16 +122,16 @@ function graph()
         scal_ext_x, scal_ext_y, scal_ext_z = get_ranges(x, y, z)
 
     # Scale data to data/interval so that the plot is unit/equal sized
-    xtickrange = range(scal_ext_x..., length=n)
-    ytickrange = range(scal_ext_y..., length=n)
-    ztickrange = range(scal_ext_z..., length=n)
+    xtickrange = range(scal_ext_x..., length = n)
+    ytickrange = range(scal_ext_y..., length = n)
+    ztickrange = range(scal_ext_z..., length = n)
     # The tick labels should still represent the original range of values
-    xticklabels = string.(range(ext_x..., length=n))
-    yticklabels = string.(range(ext_y..., length=n))
-    zticklabels = string.(range(ext_z..., length=n))
+    xticklabels = string.(range(ext_x..., length = n))
+    yticklabels = string.(range(ext_y..., length = n))
+    zticklabels = string.(range(ext_z..., length = n))
 
     ls = scene, layout = layoutscene()
-    
+
     colors = to_colormap(:RdYlGn_3, n) # Get N colors from colormap to represent response variable TODO: allow choosing colormap?
 
     # TODO: better way of knowing variable vs response columns
@@ -102,77 +156,36 @@ function graph()
         scal_z = z / range_z
         scal_plot_unit = mean((range_x, range_y, range_z))
 
-        # Draw points
-        for (i, col) in enumerate(colors)
-            scatter!(
-                s,
-                scal_x[i:i], scal_y[i:i], scal_z[i:i],
-                markersize = scal_plot_unit * 5, marker = :circle,
-                color = col,
-                show_axis = true,
-                camera = cam3d!,
-            )
-            text!(
-                s,
-                "$((x[i], y[i], z[i]))",
-                position = Point3f0(scal_x[i]+.5/range_x, scal_y[i]+.1/range_y, scal_z[i]+.2/range_z),
-                textsize = scal_plot_unit / 500,
-                color = :black,
-                rotation = 3.15,
-            )
-        end
-        
-        # Draw grid
-        # TODO: probably use some permutation function to make it more elegant
-        for var_dim_idx in 1:num_vars # scal_uniq_var_vals index of the dimension that will draw the line
-            # scal_uniq_var_vals index of the other invariant dimensions
-            invar_data_dim_idx1 = mod1(var_dim_idx + 1, 3)
-            invar_data_dim_idx2 = mod1(var_dim_idx + 2, 3)
-            for line_idx in 1:n_uniq_var_vals
-                for line_idx2 in 1:n_uniq_var_vals
-                    invar_data_dim1 = fill(scal_uniq_var_vals[invar_data_dim_idx1][line_idx], n_uniq_var_vals)
-                    invar_data_dim2 = fill(scal_uniq_var_vals[invar_data_dim_idx2][line_idx2], n_uniq_var_vals)
+        create_points_coords(s, x, y, z, range_x, range_y, range_z, scal_x, scal_y, scal_z, scal_plot_unit, colors)
 
-                    # Plot function takes in order x,y,z
-                    data = Array{Array{Float64, 1}, 1}(undef, 3)
-                    data[var_dim_idx] = scal_uniq_var_vals[var_dim_idx]
-                    data[invar_data_dim_idx1] = invar_data_dim1
-                    data[invar_data_dim_idx2] = invar_data_dim2
-                    
-                    lines!(
-                        s,
-                        data[1], data[2], data[3],
-                        linestyle = :dash,
-                        transparency = true,
-                        color = RGBA(0., 0., 0., .4),
-                        # color = colors,
-                        show_axis = true,
-                    )
-                end
-            end
-        end
+        create_grid(s, scal_uniq_var_vals, num_vars, n_uniq_var_vals)
 
-        xticks!(s.scene, xtickrange=xtickrange, xticklabels=xticklabels)
-        yticks!(s.scene, ytickrange=ytickrange, yticklabels=yticklabels)
-        zticks!(s.scene, ztickrange=ztickrange, zticklabels=zticklabels)
-        # display(s.scene)
+        xticks!(s.scene, xtickrange = xtickrange, xticklabels = xticklabels)
+        yticks!(s.scene, ytickrange = ytickrange, yticklabels = yticklabels)
+        zticks!(s.scene, ztickrange = ztickrange, zticklabels = zticklabels)
     end
-    
-    # display(scene)
-    scene
+
+    ls
 end
 
-function save_button(scene, filename)
-    scene.center = false
-    save(filename, scene)
+function create_save_button(scene, filename)
+    button = Button(scene, label = "Save")
+
+    on(button.clicks) do n
+        println("$(button.label[]) -> $filename.")
+        scene.center = false
+        save(filename, scene)
+    end
+
+    button
 end
 
 function main(args)
     filename_save = args[1]
 
-    scene = graph()
-    # save_button(scene, filename_save)
-    
+    scene, layout = create_plots()
+    save_button = create_save_button(scene, filename_save)
+
     display(scene)
 end
 
